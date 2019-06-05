dd673c74d133924c4abcc0f2f981fe45422810e5
Tersia Koetzee <tersiakoetzee@gmail.com>
2019-06-05 09:24:38 +0200

    initial commit

---
+++ greet.js
@@ -0,0 +1,22 @@
+var greetBtn = document.querySelector(".greetBtn");
+ 
+
+function greetingBtn(){
+ 
+var greet = 0;
+btn.addEventListener('click', function(){
+    greet++;
+
+    localStorage['spotted' ];
+});
+var greetingBtn = 0;
+
+if (localStorage['spotted']){
+
+    greet = Name(localStorage['spotted']);
+}   
+}
+
+
+
+greetBtn.addEventListener('click',);
