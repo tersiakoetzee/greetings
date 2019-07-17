44ca6aaf52d6d66bbdb24bae006babad68844134
Tersia Koetzee <tersiakoetzee@gmail.com>
2019-07-17 13:29:26 +0200

    fixed increment

--- greet.js
+++ greet.js
@@ -1,53 +1,28 @@
 var greetBtnElem = document.querySelector(".clicked");
 var language = document.querySelectorAll(".language");
-var test = document.getElementById("#language")
+var test = document.getElementById("#language");
 var textVal = document.querySelector("#textName");
-var greetingString = document.querySelector(".greetingDiv")
-var theLanguage = ""
+var greetingString = document.querySelector(".greetingDiv");
+var theLanguage = "";
 var greet = greetFactory();
 var namesGreeted = {};
-var greetcount = document.querySelector(".counter")
+var greetcount = document.querySelector(".counter");
 localStorage['name'] = namesGreeted;
 
 
 function greetMe() {
     var checkedRadiobtn = document.querySelector("input[name='radiobut']:checked");
-    for (let i = 0; i < checkedRadiobtn.length; i++) {
-        var elem = checkedRadiobtn[i];
-        if (elem.checked) {
-            if (elem.value == "English") {
-                document.getElementById("lang").innerHTML = greet.theLanguage;
-                increment();
-            }
-        }
-    }
+    
     if (checkedRadiobtn) {
-        theLanguage = checkedRadiobtn.value
+        theLanguage = checkedRadiobtn.value;
     }
 
+console.log(textVal.value)
+    greetingString.innerHTML = greet.getName(textVal.value, theLanguage)
+    greetcount.innerHTML = greet.setCounter();
 
-    greetingString.innerHTML = greet.getName(textVal.value)
-    increment()
 }
 
-
-
-function increment() {
-    var textVal = document.querySelector("#textName").value;
-    greet.getName(textVal);
-
-
-    if (textVal === undefined || textVal === "") {
-        return false;
-    }
-    greet.setCounter();
-    greetcount.innerHTML = greet.getCounter();
-}
-
-// localStorage.getItem('Name');
-// namesCounted++;
-
-// localStorage['counted'] = namesCounted;
 
 
 greetBtnElem.addEventListener("click", greetMe)
--- greetfactory.js
+++ greetfactory.js
@@ -1,37 +1,35 @@
 function greetFactory(listOfNames) {
 
-  var namesGreeted = {} || namesGreeted[listOfNames];
+  var namesGreeted = {} 
+  
   var greetingsCounter = 0;  
 
-  function setName(textVal) {
-    namesGreeted = textVal;
-    localStorage.setItem(textVal, 1);
-  
+
+  function name(textVal){
+    if(namesGreeted[textVal] === undefined){
+      greetingsCounter++
+
+      namesGreeted[textVal] = 0
+    }
   }
 
-  function getName(textVal) {
-
-    
-    localStorage.getItem('dataName');
+  function getName(textVal, theLanguage) {
+    name(textVal)
     var upperCaseName = textVal.charAt(0).toUpperCase() + textVal.slice(1);
     if(upperCaseName.length < 1) {
       return "No Name Entered";
     }
     if (theLanguage === "English") {
-      theGreeting = "Hello, " + upperCaseName;
+      return theGreeting = "Hello, " + upperCaseName;
     }
     if (theLanguage === "isiXhosa") {
-      theGreeting = "Molo, " + upperCaseName;
+      return theGreeting = "Molo, " + upperCaseName;
     }
     if (theLanguage === "Afrikaans") {
-      theGreeting = "Hallo, " + upperCaseName;
+      return theGreeting = "Hallo, " + upperCaseName;
     }
-    // else {
-    //   (theLanguage === undefined || textVal === "");
-    //   theGreeting = "No Name Entered";
-    // }
-
-    return theGreeting;
+  
+    
 
   }
 
@@ -40,17 +38,18 @@ function greetFactory(listOfNames) {
   }
 
   function setCounter() {
-    greetingsCounter++;
+    console.log(greetingsCounter)
+  return greetingsCounter;
   }
 
   function getCounter() {
     return localStorage.length;
+
   }
 
 
   return {
     englishLang,
-    setName,
     getName,
     setCounter,
     getCounter
@@ -58,20 +57,3 @@ function greetFactory(listOfNames) {
   }
 }
 
-
-
-
-//     var theName = textVal
-// var z = Number(theName)
-// var x = z.charAt(0)
-// console.log(typeof z)
-// console.log(typeof x)
-
-// console.log(typeof theName.charAt(2))
-//       for (var i=0;i<theName.length;i++){
-//         var aLetter = theName[i]
-//         if (typeof aLetter == "string"){
-//           alert("aNumber")
-//         }
-//       }
-//         return true
